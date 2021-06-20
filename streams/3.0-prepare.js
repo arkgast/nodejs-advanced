bfed21d4742710760b64e7d4706c2216bdb843a6
Arnold Gandarillas Castillo <arkgast@gmail.com>
2021-06-20 08:02:48 -0400

    chore: update content creation script so it fits in one line

--- streams/3.0-prepare.js
+++ streams/3.0-prepare.js
@@ -5,7 +5,7 @@ const outputStream = createWriteStream(resolve(__dirname, '..', 'input.txt'))
 
 for (let i = 0; i < 1e6; i++) {
   outputStream.write(
-    `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ${i}.\n`
+    `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${i}.\n`
   )
 }
 
